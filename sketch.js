efa307c42a3675911999f1d3cf46907cbb22ebcb
ShauryaKaushik <65485931+ShauryaKaushik@users.noreply.github.com>
2020-06-13 14:46:52 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -39,7 +39,10 @@ function setup() {
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
  	World.add(world, ground);
 
-
+       var packageBodyOptions = {
+	reinstitution = 0.5;
+       }
+       
 	Engine.run(engine);
   
 }
